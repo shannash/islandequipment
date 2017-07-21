0446d17215c3b71bcf796672c858e66bfc925f86
shannash <30345404+shannash@users.noreply.github.com>
2017-07-21 06:42:51 -0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,203 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="generator" content="Website Painter">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="contact_php.css">
+</head>
+<body>
+<div class="wsp1">
+  <div id="container_124eecc9">
+    <div id="container_124eecc9_padding" >
+      <div class="wsp1">
+        <div id="menu_75dd1732">
+          <div  class="wsp2"><a href="javascript:void(0);">
+	<div id="menuentry_4b99541d"  class="wsp3 menu_75dd1732_mainMenuEntry mobileEntry">
+		<div class="wsp4">
+            <span class="wsp5">Menu &#9660;</span>
+		</div>
+	</div>
+</a>
+<a href="attachments.html" style="text-decoration:none">
+	<div id="menuentry_1fac06a5"  class="wsp6 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp4">
+            <span class="wsp7">EQUIPMENT</span>
+		</div>
+	</div>
+</a>
+<a href="about_us.html" style="text-decoration:none">
+	<div id="menuentry_3aaa4c88"  class="wsp8 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp4">
+            <span class="wsp7">ABOUT US</span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_73dbe1f0"  class="wsp9 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp4">
+            <span class="wsp7">CONTACT US</span>
+		</div>
+	</div>
+</a>
+<a href="masterpage.html" style="text-decoration:none">
+	<div id="menuentry_1cac92e"  class="wsp10 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp11">
+            <span class="wsp12">HOME</span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_3e162b62"  class="wsp13 menu_75dd1732_mainMenuEntry mobileEntry">
+		<div class="wsp14">
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_36e9ed88"  class="wsp15 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp14">
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_37d2a949"  class="wsp16 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp14">
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_3987d456"  class="wsp17 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp4">
+            <span class="wsp18"> </span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_4da2098d"  class="wsp19 menu_75dd1732_mainMenuEntry normalEntry">
+		<div class="wsp4">
+            <span class="wsp18"> </span>
+		</div>
+	</div>
+</a>
+
+	<script type="text/javascript" src="rc_images/wsp_menu.js"></script>
+	<script type="text/javascript">
+		var js_menu_75dd1732= new wsp_menu('menu_75dd1732', 'menu_75dd1732', 10);
+
+		js_menu_75dd1732.createMenuForItem('menuentry_4b99541d', ["            <span class=\"wsp5\">Tour A</span> ", 'javascript:void(0);', '',
+		                                   "            <span class=\"wsp5\">Tour B</span> ", 'javascript:void(0);', '',
+		                                   "", 'javascript:void(0);', '',
+		                                   "            <span class=\"wsp5\">About us</span> ", 'javascript:void(0);', '',
+		                                   "            <span class=\"wsp5\">Station</span> ", 'javascript:void(0);', '']);
+		js_menu_75dd1732.createMenuForItem('menuentry_1fac06a5', ["            <span class=\"wsp20\">ATTACHMENTS</span> ", 'attachments.html', '',
+		                                   "            <span class=\"wsp20\">GENSETS</span> ", 'gensets.html', '',
+		                                   "            <span class=\"wsp21\">SHELTERS</span> ", 'shelters.html', '',
+		                                   "            <span class=\"wsp22\">SHOP EQUIPMENT</span> ", 'shop_equip.html', '',
+		                                   "            <span class=\"wsp22\">YARD EQUIPMENT</span> ", 'yard_equip.html', '',
+		                                   "            <span class=\"wsp22\">VIEW ALL EQUIPMENT</span> ", 'view_all.html', '',
+		                                   "            <span class=\"wsp22\">RECREATIONAL &amp; RESIDENTIAL</span> ", 'recreational.html', '']);
+		js_menu_75dd1732.createMenuForItem('menuentry_3aaa4c88', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_73dbe1f0', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_1cac92e', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_3e162b62', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_36e9ed88', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_37d2a949', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_3987d456', []);
+		js_menu_75dd1732.createMenuForItem('menuentry_4da2098d', []);
+
+	</script>
+            </div>
+          </div>
+        <span class="wsp23">&#169;<br/></span>
+        <span class="wsp24"><br/><br/></span>
+        </div>
+      <div style="clear:both"></div>
+      </div>
+    </div>
+  <h1 id="heading_10c2f3b5">
+    <div class="wsp1">
+      <div id="text_478fcc2">
+        <div class="wsp1">
+          <span class="wsp25">BRINGING VALUE FROM THE PRARIES TO THE ISLAND !</span>
+          <div id="container_594c6e66">
+            <div id="container_594c6e66_padding" >
+              </div>
+            </div>
+          </div>
+        </div>
+      <span class="wsp26">ISLAND EQUIPMENT</span>
+      </div>
+    </h1>
+  <div id="text_b8e6461">
+    <div class="wsp1">
+      <span class="wsp27"><br/><br/>VANCE MICHENER <br/><br/>EDMONTON780-777-6547<br/>BC                 250-777-6547<br/><br/>MATT            </span>
+      </div>
+    </div>
+  <form  action="contact.php"
+enctype="application/x-www-form-urlencoded"
+method="POST" id="form_d41b8df">
+    <div id="form_d41b8df_padding" >
+      <div class="wsp1">
+<textarea name="TextEdit3" title="" cols="32" rows="6"  id="edit_2accbfac"></textarea>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_32cdd952" >
+        <div id="text_78911a8e">
+          <div class="wsp1">
+            <span class="wsp28">  Email :</span>
+            </div>
+          </div>
+        <div id="text_7215db98">
+          <div class="wsp1">
+            <span class="wsp29">CONTACT US</span>
+            </div>
+          </div>
+        <div id="text_e185fff">
+          <div class="wsp1">
+            <span class="wsp28"> Name :</span>
+            </div>
+          </div>
+<input type="text" value="" title="" name="TextEdit2"  id="edit_59d6beb3" >
+        <div id="text_7c09b130">
+          <div class="wsp1">
+            <span class="wsp30">MESSAGE :</span>
+            </div>
+          </div>
+        <div id="text_48e06044">
+          <div class="wsp1">
+            <span class="wsp28"> Phone Number :</span>
+            </div>
+          </div>
+<input type="text" value="" title="" name="TextEdit4"  id="edit_eb0c707" >
+<div id="php_1b65c2fa"><?PHP
+$text = "";
+foreach($_POST as $name => $value) 
+{
+  $text .= "$name : $value\n";
+}
+
+if ($text != "")
+{
+  echo 'Thanks for contacting us.';
+  mail("kozshann@gmail.com", "Contact form request", $text);
+}
+		
+?></div>
+<input name="Button1" type="submit" value="SEND" title=""  id="button_29a689e6" >
+        <img src="rc_images/ph_animate.gif" id="img_30cd4bd2" alt="" title="" />
+        </div>
+      <div style="clear:both"></div>
+      </div>
+    </form>
+  </div>
+<div style="text-align:center; position:fixed; bottom:0px; right:0px;
+left:0px; height:30px; background-color:#dddddd; border-top:1px solid  
+#666666; padding:15px; font-size:10pt; font-family:Arial, Helvetica, sans-serif;" id="wsparea51">Created with an unregistered version of <a target="_blank" style="color:#000000;"
+href="http://www.ambiera.com/websitepainter/">WebsitePainter</a><br/><small>(This text will not be visible anymore  
+once you <a target="_blank" href="http://www.ambiera.com/websitepainter/buy.html" style="color:#000000;">buy</a> WebsitePainter)</small></div>
+<script type="text/javascript">
+wspAreaSetAlpha=function(a,c){c.style.filter="Alpha(Opacity="+a*100+")";c.style.MozOpacity=a;c.style.opacity=a};
+var b=document.getElementById("wsparea51"),d=(new Date).getTime(),e=0;if(b!=0){wspAreaSetAlpha(0,b);
+e=setInterval(function(){var a=((new Date).getTime()-d)/3E3;if(a>1){wspAreaSetAlpha(1,b);clearInterval(e)}else wspAreaSetAlpha(a,b)},100)};
+</script>
+</body>
+</html>
